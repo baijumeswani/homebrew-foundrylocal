565227035d8e77ebe5cd34d4083a22c046229e94
Baiju Meswani <baijumeswani@gmail.com>
2025-07-17 13:50:33 -0700

    Update foundrylocal.rb

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -4,8 +4,7 @@ class Foundrylocal < Formula
   url "https://github.com/baijumeswani/homebrew-foundrylocal/releases/download/v0.5.108/FoundryLocal-osx-arm64-0.5.108.zip"
   sha256 "57fb18eae2437dc5fccb763d622958dec4da573effecde097db3b958860b8a73"
 
-  preserve_mach_o "bin/libonnxruntime-genai.dylib"
-  preserve_mach_o "bin/libonnxruntime.dylib"
+  skip_clean :all
   
   def install
     prefix.install Dir["bin/foundry"]
