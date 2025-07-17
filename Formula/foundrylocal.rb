6fcf0d98a58a1baeb167e15df4e0f60afee9c1e5
Baiju Meswani <baijumeswani@gmail.com>
2025-07-17 13:58:58 -0700

    Update foundrylocal.rb

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -4,6 +4,7 @@ class Foundrylocal < Formula
   url "https://github.com/baijumeswani/homebrew-foundrylocal/releases/download/v0.5.108/FoundryLocal-osx-arm64-0.5.108.zip"
   sha256 "57fb18eae2437dc5fccb763d622958dec4da573effecde097db3b958860b8a73"
 
+  bottle :unneeded
   skip_clean :all
   
   def install
