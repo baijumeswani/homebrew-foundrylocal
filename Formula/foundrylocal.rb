6c512c8ffb7e2c4077f282f003dc0bfcfe75fccc
Baiju Meswani <baijumeswani@gmail.com>
2025-07-17 14:04:22 -0700

    Update foundrylocal.rb

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -3,17 +3,14 @@ class Foundrylocal < Formula
   homepage "https://github.com/microsoft/Foundry-Local"
   url "https://github.com/baijumeswani/homebrew-foundrylocal/releases/download/v0.5.108/FoundryLocal-osx-arm64-0.5.108.zip"
   sha256 "57fb18eae2437dc5fccb763d622958dec4da573effecde097db3b958860b8a73"
-
-  bottle :unneeded
-  skip_clean :all
   
   def install
-    prefix.install Dir["bin/foundry"]
-    prefix.install Dir["bin/Inference.Service.Agent"]
-    prefix.install Dir["bin/libonnxruntime-genai.dylib"]
-    prefix.install Dir["bin/libonnxruntime.dylib"]
-    prefix.install Dir["bin/appsettings.json"]
-    
-    bin.install_symlink prefix/"foundry"
+    bin.install "bin/foundry"
+    bin.install "bin/Inference.Service.Agent"
+
+    lib.install "bin/libonnxruntime-genai.dylib"
+    lib.install "bin/libonnxruntime.dylib"
+
+    etc.install "bin/appsettings.json"
   end
 end
